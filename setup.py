c9c91322b0e123b9188d39571b9090b60daec253
grahamcarlyle <grahamcarlyle>
2004-07-25 18:22:08 +0000

    - added info for PyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,34 @@
+"""pMock: Python mock object framework.
+
+pMock provides support for creating mock objects for use in unit testing.
+The api is modelled on the jmock mock object framework.
+"""
+
+classifiers = """\
+Development Status :: 3 - Alpha
+Intended Audience :: Developers
+License :: OSI Approved :: Python Software Foundation License
+Programming Language :: Python
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: Software Development :: Testing
+Operating System :: OS Independent
+"""
+
 from distutils.core import setup
+
+doclines = __doc__.split("\n")
 
 setup (
     name = "pmock",
-    version = "0.2",
-    description = "pmock - a mock objects framework for Python",
+    version = "0.3",
+    maintainer="Graham Carlyle",
+    maintainer_email="grahamcarlyle@users.sourceforge.net",
+    license="Same terms as Python",
+    platforms = ["any"],
+    url = "http://pmock.sf.net",
+    description = doclines[0],
+    classifiers = filter(None, classifiers.split("\n")),
+    long_description = "\n".join(doclines[2:]),
     package_dir = {"": "src"},
     py_modules = ["pmock"]
     )
-      
-      
